9c7eeba20072146f8cccffc3ee208372fbff5741
TheBaronMc <baron_mc@yahoo.com>
2025-06-20 18:57:01 +0200

    Change 404 page

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,8 +1,19 @@
+import './404.scss';
+import { A } from '@solidjs/router';
+
 export default function NotFound() {
   return (
-    <section class="text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">404: Not Found</h1>
-      <p class="mt-4">It's gone 😞</p>
-    </section>
+    <div class="error">
+      <div class="error-image">
+        <img src="/images/chronometer.png" alt="Chronometer" />
+        <div class="text">404</div>
+      </div>
+      <div class="error-message">
+        <div>Not Found</div>
+        <A href="/" class="button">
+          Go Home
+        </A>
+      </div>
+    </div>
   );
 }
