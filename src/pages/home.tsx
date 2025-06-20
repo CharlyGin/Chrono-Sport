90a0ea1a24651ba6e6a81297f3d6d96e50c03fc3
TheBaronMc <baron_mc@yahoo.com>
2025-06-20 15:40:35 +0200

    Add animation on home screen

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import './home.scss';
 import { A } from '@solidjs/router';
-import { For } from 'solid-js';
+import { For, onMount } from 'solid-js';
 import { routes } from '../routes';
 
 export default function Home() {
@@ -24,8 +24,32 @@ export default function Home() {
     })
     .sort((a, b) => a.name.localeCompare(b.name));
 
+  let menuRef: HTMLDivElement | undefined = undefined;
+
+  onMount(() => {
+    if (!menuRef) {
+      return;
+    }
+
+    let incr: number = 1;
+    for (const menu_section of menuRef.querySelectorAll('.menu-section')) {
+      const title: HTMLElement = menu_section.querySelector('.menu-section-title');
+      title.style.animationDelay = `${incr * 0.1}s`;
+
+      incr++;
+
+      const items = menu_section.querySelectorAll<HTMLElement>(
+        '.menu-section-item-list .menu-section-item'
+      );
+      for (const item of items) {
+        item.style.animationDelay = `${incr * 0.1}s`;
+        incr++;
+      }
+    }
+  });
+
   return (
-    <div class="menu">
+    <div class="menu" ref={menuRef}>
       <div class="menu-section">
         <div class="menu-section-title">classic</div>
         <div class="menu-section-item-list">
