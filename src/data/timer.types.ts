7aa0664b5c55b8a9f87909371aee08cbfeeabb49
TheBaronMc <baron_mc@yahoo.com>
2025-11-07 21:51:07 +0100

    #8 Refactor Timer ♻️

---
+++ src/data/timer.types.ts
@@ -0,0 +1,22 @@
+export type TimerState = 'ready' | 'running' | 'pause';
+
+export type Chrono = {
+  name: string;
+  color?: number;
+  time: number;
+};
+
+export type Round = {
+  pre?: number;
+  name: string;
+  chronos: Chrono[];
+  post?: number;
+  repeat?: number;
+};
+
+export type Timer = {
+  name: string;
+  begin?: number;
+  rounds: Round[];
+  end?: number;
+};
